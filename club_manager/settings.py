7ed086e37f6ff2348a472b006e8484e27b35da59
bdhess13 <bhess@uwm.edu>
2026-09-11 23:39:02 -0500

    Simple login page
    Created login page with simple css, does not have any users created or authorized so any inputs push through to the 'dashboard'

--- club_manager/settings.py
+++ club_manager/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'club_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'club_manager'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Email
