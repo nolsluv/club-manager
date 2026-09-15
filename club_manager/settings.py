c20b19e981edc99b9ad2dcc26ba7aa8daa2d851f
bdhess13 <bhess@uwm.edu>
2026-09-14 19:53:20 -0500

    Login page
    Fixed login page to be a template and flow with the main branch, added css to make login page look nice and blend with previous pages

--- club_manager/settings.py
+++ club_manager/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'club_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'club_manager', BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
