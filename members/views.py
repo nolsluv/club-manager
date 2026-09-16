a48d6e70b6a71e6038952342df1b63506ab75bdb
nolsluv <129223611+nolsluv@users.noreply.github.com>
2026-09-15 19:30:17 -0700

    modified form rendering to refer to correct regiter.html filepath

--- members/views.py
+++ members/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import RegisterForm
 from .models import Member
@@ -8,3 +8,16 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            user = form.save()
+            Member.objects.create(
+                first_name=request.POST.get('first_name', ''),
+                last_name=request.POST.get('last_name', ''),
+                email=user.email,
+            )
+            login(request, user)
+            return redirect('home')
+
+        else:
+            form = RegisterForm()
+
+        return render(request, 'register.html', {'form': form})
